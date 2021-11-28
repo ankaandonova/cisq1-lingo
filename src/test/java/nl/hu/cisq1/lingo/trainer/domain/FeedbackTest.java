ce10ee29e8517cfb674d2652c5c56f14fc3e20c8
ankaandonova <anka.andonova@student.hu.nl>
2021-11-28 22:04:20 +0100

    update ci pipline

---
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -0,0 +1,14 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeedbackTest {
+    @Test
+    void generateFeedback(){
+
+
+    }
+
+}
